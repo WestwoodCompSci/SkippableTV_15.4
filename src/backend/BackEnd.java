e6a7bd58f4239158f22c70172ce3b6c7a2da33e1
unknown <s510758@4WF4622.rrisd.local>
2015-04-30 15:41:59 -0500

    working backend

--- src/backend/BackEnd.java
+++ src/backend/BackEnd.java
@@ -3,7 +3,7 @@ package backend;
 import java.util.ArrayList;
 
 import org.json.JSONObject;
-
+import backend.Series;
 import networking.Access;
 
 public class BackEnd {
@@ -31,6 +31,11 @@ public class BackEnd {
 			return 5;
 =======
 	}
+	public static void main(String [] args)
+	{
+		BackEnd b = new BackEnd();
+		System.out.println(b.series.get(0).title);
+	}
 	public ArrayList<Series> getSeries()
 	{
 		ArrayList<Series> s=new ArrayList<Series>();
@@ -39,7 +44,7 @@ public class BackEnd {
 		{
 			JSONObject temp =a.getShow(j);
 			Series t =new Series(temp.getString("name"),temp.getString("total length"),temp.getInt("id"),temp.getInt("episodes"),temp.getInt("seasons"));
-			series.add(t);
+			s.add(t);
 			
 		}
 		return s;
--- src/backend/Series.java
+++ src/backend/Series.java
@@ -1,6 +1,7 @@
 package backend;
 
 import java.util.ArrayList;
+import backend.Season;
 
 public class Series {
 
