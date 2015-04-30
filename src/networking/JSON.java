4b75bbd1370a7d6550d78d740396b2c4aa73aa47
boxboiatx <s410897@148JS22.rrisd.local>
2015-04-30 14:45:49 -0500

    updated url stuff

--- src/networking/JSON.java
+++ src/networking/JSON.java
@@ -1,28 +1,29 @@
 package networking;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 
 
 public class JSON {
 	
-	
+	JSONObject obj;
 	
 	public JSON() {
 		URLConnect con = new URLConnect("http://preview.qbizlbk2gkua0pb93crxph878kcvj9k957h3bwfu5v0u23xr.box.codeanywhere.com/web/rest/v1/");
 
 		String parse = con.sendPost("getUser.php?user_id=1", "");
 		
-		JSONObject obj = new JSONObject(parse);
+		 obj = new JSONObject(parse);
 		
 		
-		JSONObject obj2 =  obj.getJSONObject("user");
-		//if obj2 instanceof JSONObject
-		System.out.println(obj2.toString());
-		System.out.println(obj2.getString("ratings"));
+		
 		
 	}
+	
+	public JSONObject getObject() {
+		return obj;
+	}
+	
 	
 	
 	
