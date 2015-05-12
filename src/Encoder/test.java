07d931c4d98427240e016ad5fafcdecd957aee3d
Tommy Baxter <tommyjbaxter@gmail.com>
2015-05-12 14:45:17 -0500

    Update to make test class easier to read

--- src/Encoder/test.java
+++ src/Encoder/test.java
@@ -2,26 +2,30 @@ package Encoder;
 
 public class test {
 	private Encoder e =  new Encoder();
+	private String x = "";
 	
 	public test(String s){
 		System.out.println("Starting String: " + s);
 		byte[] temp = e.encrypt(s);
 		System.out.println("Encrypted String: ");
 		for(byte b : temp){
-			System.out.println(b);
+			x += b;
 		}
-		String x = e.decrypt(temp);
+		System.out.println(x);
+		x = e.decrypt(temp);
 		System.out.println("Decrypted String: " + x);
 		temp = e.encryptOneWay(s);
+		x = "";
 		System.out.println("One Way Encrypted String: ");
 		for(byte b : temp){
-			System.out.println(b);
+			x += b;
 		}
+		System.out.println(x);
 		x = e.decrypt(temp);
 		System.out.println("Smash bois");
 	}
 	
 	public static void main(String[] args){
-		new test("Dingus");
+		new test("Skippable.tv");
 	}
 }
