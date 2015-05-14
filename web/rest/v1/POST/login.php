1ee205177428b39e0cd7c786a7d8dc7f9b3b5db7
= <=>
2015-05-14 17:04:58 -0400

    Started login

--- web/rest/v1/POST/login.php
+++ web/rest/v1/POST/login.php
@@ -5,7 +5,42 @@ $requirements = array(
 	"username",
 	"password"
 );
+foreach($requirements as $requirement)
+{
+	if(!isset($_POST[$requirement]))
+		die(json_encode(array("error" => 1, "status" => 403,"errors" => array("Bad data"))));
+}
 
+$username = $_POST['username'];
+$password = $_POST['password'];
 
+if(preg_match('/\s/',$username))
+	die(json_encode(array("error" => 1, "status" => 403,"errors" => array("Username contains spaces"))));
 
+$connection = mysqli_connect(config::$database["host"],config::$database["username"],config::$database["password"],config::$database["database"]) 
+	or die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Could not connect to databsase"))));
+$query = "SELECT `id` FROM `Users` WHERE `username` = $username";
+
+$result = mysqli_query($connection,$query)
+	or die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Query error checking username"))));
+switch(mysqli_num_rows($result)):
+	case 0:
+		die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Username does not exist"))));
+	case 1:
+		cont();
+	default:
+		die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Multiple usernames exist. Contact administrator"))));
+
+function cont()
+{
+	$password = crypt($password);
+	$id = mysqli_fetch_array($result);
+	$id = $id["id"];
+	settype($id,"integer");
+	$query = "SELECT * FROM `Passwords` WHERE `id` = $id";
+	$result mysqli_query($connection,$query) or 
+		die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Could not get username from password database"))));
+	if(mysqli_num_rows($result) == 1)
+	
+}
 ?>
