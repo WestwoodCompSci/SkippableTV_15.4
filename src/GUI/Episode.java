1994950c05cba64ac2fa43a3f8e13c6e284e74b5
William Li <willdude3456@gmail.com>
2015-05-20 16:10:08 -0500

    adfasdfasdfasdf

---
+++ src/GUI/Episode.java
@@ -0,0 +1,10 @@
+package GUI;
+
+public class Episode {
+	private int x;
+	private int y;
+	
+	private int rating;
+	
+	private boolean skip;
+}
---
+++ src/GUI/Season.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class Season {
+	private int x;
+	private int y;
+	
+}
