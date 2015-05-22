8a319a5bae25849e017e2bc05bd2f4157625f08e
= <=>
2015-05-22 10:50:37 -0400

    Fixed bugs in addUser. Now works properly.

--- web/rest/v1/POST/addUser.php
+++ web/rest/v1/POST/addUser.php
@@ -13,7 +13,7 @@ $requirements = array(
 foreach($requirements as $requirement)
 {
 	if(!isset($_POST[$requirement]))
-		die(json_encode(array("error" => 1, "status" => 403,"errors" => array("Bad data"))));
+		die(json_encode(array("error" => 1, "status" => 403,"errors" => array("Bad data $requirement"))));
 	if(preg_match('/\s/',$requirement))
 		die(json_encode(array("error" => 1, "status" => 403,"errors" => array("$requirement contains spaces"))));
 }
@@ -33,16 +33,21 @@ $result = mysqli_query($connection,$query)
 	or die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Query error checking if username exists"))));
 if(mysqli_num_rows($result) > 0)
 	die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Username exists"))));
-$query = "INSERT INTO `Users` (`username`, `privileges`, `Profile URI`, `email`, `fname`, `lname`, `birthday`) VALUES ({$username},0,{$picture},{$email},{$fname},{$lname},{$bday})";
-$result = mysqli_query($connection,$query);
-if(mysqli_affected_rows($result) > 0)
+$query = "INSERT INTO `Users` (`username`, `privileges`, `Profile URI`, `email`, `fname`, `lname`, `birthday`) VALUES (\"{$username}\",0,\"{$picture}\",\"{$email}\",\"{$fname}\",\"{$lname}\",{$bday})";
+//print $query;
+$res = mysqli_query($connection,$query)
+	or die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Query error E-3"))));
+if(mysqli_affected_rows($connection) > 0)
 {
-	$data = mysqli_fetch_array($result);
+	$query = "SELECT * FROM `Users` WHERE `Username`=\"{$username}\" AND `fname`=\"{$fname}\"";
+	$info = mysqli_query($connection,$query)
+		or die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Could not retreive information"))));
+	$data = mysqli_fetch_array($info);
 	$salt = crypt(rand(0,1887).$username.$password);
 	$password = crypt($password,$salt);
 	$id = $data['id'];
-	$query = "INSERT INTO `Passwords` (`id`,`password`,`salt`) VALUES ($id,$password,$salt)";
-	die(json_encode(array("error" => 1, "status" => 200,"errors" => null,"user_id"=>$data['id'])));
+	$query = "INSERT INTO `Passwords` (`id`,`password`,`salt`) VALUES ($id,\"$password\",\"$salt\")" or die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Error saving password"))));;
+	die(json_encode(array("error" => 1, "status" => 200,"errors" => null,"data"=>$data)));
 }
 else die(json_encode(array("error" => 1, "status" => 500,"errors" => array("Error inserting"))));
 ?>
