fdee4bf74e5d7bf40e316f99a8aa3040d865bd6b
boxboiatx <chris.ayoubtexas@gmail.com>
2015-05-26 22:57:09 -0500

    finished and organized networking API

--- src/networking/Access.java
+++ src/networking/Access.java
@@ -20,55 +20,103 @@ public class Access {
 
 	public Access() {	
 		con = new URLConnect("http://preview.qbizlbk2gkua0pb93crxph878kcvj9k957h3bwfu5v0u23xr.box.codeanywhere.com/web/rest/v1/");
+		//URL of server with the database
 	}
-
-	public JSONObject getUserData(int userID) {
-		String parse = con.sendGet("getUser.php?user_id="+userID);
-		JSONObject obj = new JSONObject(parse).getJSONObject("user");
-		return obj;
-	}
-
-	public void addRating(int id, String hash, int season, int show, int episode, int rating, String comment) {
+	
+	/** In order to editEpisode, you must supply any original data that is unchanged back to this method.
+	 */
+	public String editEpisode(int episode_id, int season_parent, int show_parent, String name, String length, int episode_number, String username, int user_id, String hash) {
 		ArrayList<String> post = new ArrayList<String>();		
-		post.add("is_post=1&");
+		post.add("ispost=1&");
+		post.add("ep_id="+episode_id+"&");		
+		post.add("season_parent="+season_parent+"&");
+		post.add("show_parent="+show_parent+"&");
+		post.add("name="+name+"&");
+		post.add("length="+length+"&");
+		post.add("episode_number="+episode_number+"&");
+		post.add("username="+username+"&");
+		post.add("id="+user_id+"&");
+		post.add("hash="+hash);		
+		return con.sendPost("POST/editEpisode.php", post);
+	}
+	
+	
+	/** In order to editSeries, you must supply any original data that is unchanged back to this method.
+	 */
+	public String editSeries(int id, String name, int seasons, int episodes, String genre, String length, String username, int user_id, String hash) {
+		ArrayList<String> post = new ArrayList<String>();		
+		post.add("ispost=1&");
+		post.add("ep_id="+id+"&");	
+		post.add("name="+name+"&");
+		post.add("seasons="+seasons+"&");
+		post.add("episodes="+episodes+"&");
+		post.add("genre="+genre+"&");
+		post.add("total length="+length+"&");	
+		post.add("username="+username+"&");
+		post.add("id="+user_id+"&");
+		post.add("hash="+hash);		
+		return con.sendPost("POST/editShow.php", post);
+	}
+	
+	public String editPassword(int id, String password, String username) {
+		ArrayList<String> post = new ArrayList<String>();		
+		post.add("ispost=1&");
 		post.add("id="+id+"&");
-		post.add("hash="+hash+"&");
+		post.add("password="+password+"&");
+		post.add("username="+username);
+		return con.sendPost("POST/editPassword.php", post);
+	}
+	/** In order to editUser, you must supply any original data that is unchanged back to this method. Do NOT change playlists, ratings, history, progress or hash. Use stored values.
+	 */
+	public String editUser(int id, String username, int privileges, String Profile_URI, String email, String fname, String lname, String birthday, String playlists, String ratings, String history, String progress, String hash) {
+		ArrayList<String> post = new ArrayList<String>();		
+		post.add("id="+id+"&");		
+		post.add("username="+username+"&");
+		post.add("privileges="+privileges+"&");
+		post.add("Profile URI="+Profile_URI+"&");
+		post.add("email="+email+"&");
+		post.add("fname="+fname+"&");
+		post.add("lname="+lname+"&");
+		post.add("birthday="+birthday+"&");
+		post.add("playlists="+playlists+"&");
+		post.add("ratings="+ratings+"&");
+		post.add("history="+history+"&");
+		post.add("progress="+progress+"&");
+		post.add("hash="+hash);
+		
+		return con.sendPost("POST/editUser.php", post);
+	}
+	/** In order to editRating, you must supply any original data that is unchanged back to this method.
+	 */
+	public String editRating(int season, int show, int episode, int rating, String comment, String username, int user_id, String hash) {
+		ArrayList<String> post = new ArrayList<String>();	
+		post.add("ispost=1&");
 		post.add("season="+season+"&");
 		post.add("show="+show+"&");
 		post.add("episode="+episode+"&");
 		post.add("rating="+rating+"&");
-		if(comment != null) {
-			comment = comment.replace(" ", "%20");
-			post.add("comment="+comment+"&");
-		}
-		else {
-			post.add("comment="+comment+"&"); }
-		con.sendPost("POST/addRating.php", post);
-	}
-
-	public boolean addUser(String username, String password, String firstname, String lastname, String email, String birthday, String pictureURL) {
-
-		ArrayList<String> post = new ArrayList<String>();		
-		post.add("is_post=1&");
+		post.add("comment="+comment+"&");
 		post.add("username="+username+"&");
-		post.add("password="+password+"&");
-		post.add("email_address="+email+"&");
-		post.add("fname="+firstname+"&");
-		post.add("lname="+lastname+"&");
-		post.add("bithday="+birthday+"&");
-		if(pictureURL != null) {
-			pictureURL = pictureURL.replace(" ", "%20");
-			post.add("profile_uri="+pictureURL);
-		}
-		else {
-			
-		}
-		if(con.sendPost("POST/addUser.php", post).contains("\"error\":1")) {
-			return false;
-		}
-		return true;
+		post.add("id="+user_id+"&");
+		post.add("hash="+hash);
+		return con.sendPost("POST/editRating.php", post);
 	}
-
+	/** In order to editSeason, you must supply any original data that is unchanged back to this method.
+	 */
+	public String editSeason(int parent, int number, int episodes, String total_length, int season_id, String username, int user_id, String hash) {
+		ArrayList<String> post = new ArrayList<String>();	
+		post.add("ispost=1&");
+		post.add("parent="+parent+"&");
+		post.add("number="+number+"&");
+		post.add("episodes="+episodes+"&");
+		post.add("total length="+total_length+"&");
+		post.add("season_id="+season_id+"&");
+		post.add("username="+username+"&");
+		post.add("id="+user_id+"&");
+		post.add("hash="+hash);
+		return con.sendPost("POST/editSeason.php", post);
+	}
+	
 	public JSONObject login(String username, String password) {
 		ArrayList<String> post = new ArrayList<String>();		
 		post.add("ispost=1&");
@@ -78,29 +126,6 @@ public class Access {
 
 	}
 
-	public ArrayList<Integer> getShows() {
-		String parse = con.sendGet("findShow.php?show_name=");
-		JSONArray obj = new JSONObject(parse).getJSONArray("shows");
-
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i = 0; i < obj.length(); i++) {
-			list.add(obj.getJSONObject(i).getInt("id"));
-		}
-		return list;
-	}
-
-	public JSONObject getShow(int ID) {
-
-		String parse = con.sendGet("getShow.php?show_id="+ID);
-		try {
-			JSONObject obj = new JSONObject(parse).getJSONArray("show").getJSONObject(0);
-			return obj;
-		} catch (JSONException e){
-			e.printStackTrace();
-			return null;
-		}
-
-	}
 	public String logout(String username, int id, String hash) { 
 		ArrayList<String> post = new ArrayList<String>();		
 		post.add("ispost=1&");
@@ -182,6 +207,37 @@ public class Access {
 
 	}
 
+
+	public ArrayList<Integer> getShows() {
+		String parse = con.sendGet("findShow.php?show_name=");
+		JSONArray obj = new JSONObject(parse).getJSONArray("shows");
+
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < obj.length(); i++) {
+			list.add(obj.getJSONObject(i).getInt("id"));
+		}
+		return list;
+	}
+
+	public JSONObject getShow(int ID) {
+
+		String parse = con.sendGet("getShow.php?show_id="+ID);
+		try {
+			JSONObject obj = new JSONObject(parse).getJSONArray("show").getJSONObject(0);
+			return obj;
+		} catch (JSONException e){
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public JSONObject getUserData(int userID) {
+		String parse = con.sendGet("getUser.php?user_id="+userID);
+		JSONObject obj = new JSONObject(parse).getJSONObject("user");
+		return obj;
+	}
+	
 	public String addSeason(Season s) {
 		ArrayList<String> post = new ArrayList<String>();		
 		post.add("is_post=1&");
@@ -225,15 +281,59 @@ public class Access {
 		return con.sendPost("POST/addEpisode.php", post);
 	}
 
+	public String addRating(int id, String hash, int season, int show, int episode, int rating, String comment) {
+		ArrayList<String> post = new ArrayList<String>();		
+		post.add("is_post=1&");
+		post.add("id="+id+"&");
+		post.add("hash="+hash+"&");
+		post.add("season="+season+"&");
+		post.add("show="+show+"&");
+		post.add("episode="+episode+"&");
+		post.add("rating="+rating+"&");
+		if(comment != null) {
+			comment = comment.replace(" ", "%20");
+			post.add("comment="+comment+"&");
+		}
+		else {
+			post.add("comment="+comment+"&"); }
+		return con.sendPost("POST/addRating.php", post);
+	}
+	
+	public boolean addUser(String username, String password, String firstname, String lastname, String email, String birthday, String pictureURL) {
+
+		ArrayList<String> post = new ArrayList<String>();		
+		post.add("is_post=1&");
+		post.add("username="+username+"&");
+		post.add("password="+password+"&");
+		post.add("email_address="+email+"&");
+		post.add("fname="+firstname+"&");
+		post.add("lname="+lastname+"&");
+		post.add("bithday="+birthday+"&");
+		if(pictureURL != null) {
+			pictureURL = pictureURL.replace(" ", "%20");
+			post.add("profile_uri="+pictureURL);
+		}
+		else {
+			post.add("profile_uri=");
+		}
+		if(con.sendPost("POST/addUser.php", post).contains("\"error\":1")) {
+			return false;
+		}
+		return true;
+	}
+
+	
 	public void updateReferences() {
 		//TODO 
 		//code to update references
 		//actually dont do this just update when adding episode and stuff
 	}
-
+	/** Here was some testing code I was doing and yeah...!
+	 * 
+	 */
 	public static void main(String[] args) throws AWTException, IOException {
 		Access ax = new Access();
-
+		
 		System.out.println("add season test");
 		//System.out.println(ax.addSeason(new Season("56:44:27",1,4,555,27)));
 
